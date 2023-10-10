d7897854f1b2f4c52389555f37ade7dd1e4f7ba5
Alejo Boga <alejoboga20@gmail.com>
2023-10-10 16:16:23 -0300

    state of component

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -1,3 +1,14 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+	methods: {},
+	watch: {},
+	computed: {},
+	data() {
+		return {
+			message: 'Hello World!',
+			quote: "I'm a quote from the data object",
+			author: "I'm the author of the quote",
+		};
+	},
+});
 
 app.mount('#app');
