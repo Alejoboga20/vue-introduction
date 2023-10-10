0fce01148312ebbe34eaf77269af2f55876fae8e
Alejo Boga <alejoboga20@gmail.com>
2023-10-10 13:31:50 -0300

    style binding and class binding

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 /* Create a new Vew instance: The root of the application */
 const app = new Vue({
 	el: '#app',
+	/* Data representation */
 	data: {
 		product: 'Socks',
-		image: './vue-socks.jpeg',
+		image: './vue-green-socks.jpeg',
 		altText: 'A pair of socks',
 		onSale: true,
 		inventory: 100,
+		inStock: true,
 		details: ['80% cotton', '20% polyester', 'Gender-neutral'],
 		variants: [
-			{ variantId: 1, variantColor: 'green', variantImage: './vue-socks.jpeg' },
+			{ variantId: 1, variantColor: 'green', variantImage: './vue-green-socks.jpeg' },
 			{ variantId: 2, variantColor: 'blue', variantImage: './vue-blue-socks.jpeg' },
 		],
 		sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+		cart: 0,
+	},
+	/* Handlers */
+	methods: {
+		addToCart: function () {
+			this.cart += 1;
+		},
+		updateProduct: function (variantImage) {
+			this.image = variantImage;
+		},
 	},
 });
