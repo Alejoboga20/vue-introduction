44beda40dec3e680dd6dd9833b2c70f3abc81054
Alejo Boga <alejoboga20@gmail.com>
2023-10-10 14:39:28 -0300

    work with forms

--- main.js
+++ main.js
@@ -42,6 +42,21 @@ Vue.component('product', {
         <button v-on:click="addToCart" :disabled="!inStock" :class="{disabledButton: !inStock}">
           Add to cart
         </button>
+
+        <product-review @review-submitted="addReview"></product-review>
+
+        <div class="reviews">
+          <h2><b>Reviews</b></h2>
+          <p v-if="!reviews.length">There are no reviews yet.</p>
+
+          <ul>
+            <li v-for="review in reviews">
+              <p>{{review.name}}</p>
+              <p>Rating: {{review.rating}}</p>
+              <p>{{review.review}}</p>
+            </li>
+          </ul>
+        </div>
       </div>
 		</div>
   `,
@@ -69,6 +84,7 @@ Vue.component('product', {
 				},
 			],
 			sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+			reviews: [],
 		};
 	},
 	/* Handlers */
@@ -78,6 +94,9 @@ Vue.component('product', {
 		},
 		updateProduct: function (index) {
 			this.selectedVariant = index;
+		},
+		addReview(productReview) {
+			this.reviews.push(productReview);
 		},
 	},
 	/* Computed properties act like calculator */
@@ -97,6 +116,68 @@ Vue.component('product', {
 				return 'Free';
 			} else {
 				return 2.99;
+			}
+		},
+	},
+});
+
+Vue.component('product-review', {
+	template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{error}}</li>
+        </ul>
+      </p>
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name"/>
+      </p>
+      <p>
+        <label for="review">Review:</label>
+        <input id="review" v-model="review"/>
+      </p>
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit" />
+      </p>
+    </form>
+  `,
+	data() {
+		return {
+			name: null,
+			review: null,
+			rating: null,
+			errors: [],
+		};
+	},
+	methods: {
+		onSubmit() {
+			if (this.name && this.review && this.rating) {
+				const productReview = {
+					name: this.name,
+					review: this.review,
+					rating: this.rating,
+				};
+				this.$emit('review-submitted', productReview);
+				this.name = null;
+				this.review = null;
+				this.rating = null;
+			} else {
+				if (!this.name) this.errors.push('Name required.');
+				if (!this.review) this.errors.push('Review required.');
+				if (!this.rating) this.errors.push('Rating required.');
 			}
 		},
 	},
